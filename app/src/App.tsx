2fedda002dfc6b130648cfc85084dbbe61f17ce6
Alexander Olsen <aolsenjazz@gmail.com>
2025-11-24 17:50:21 -0500

    initial

---
+++ app/src/App.tsx
@@ -0,0 +1,37 @@
+import { useAppDispatch, useAppSelector } from '@/app/hooks';
+import { increaseBy, increment } from '@/features/counter/counter-slice';
+
+import type { UserRow } from '@vault/core/types';
+
+import './App.css';
+
+const a: UserRow = {} as unknown as UserRow;
+console.log(a);
+
+function App() {
+  const dispatch = useAppDispatch();
+  const count = useAppSelector((state) => state.counter.value);
+
+  return (
+    <main className='app'>
+      <h1>Vault.fm Boilerplate</h1>
+      <p>React + TypeScript + Vite + Redux Toolkit</p>
+
+      <div className='button-group'>
+        <button className='btn' onClick={() => dispatch(increment())}>
+          +1
+        </button>
+
+        <button className='btn' onClick={() => dispatch(increaseBy(5))}>
+          +5
+        </button>
+      </div>
+
+      <p className='count'>
+        Count: <strong>{count}</strong>
+      </p>
+    </main>
+  );
+}
+
+export default App;

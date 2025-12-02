24625dd64f0ca3da6bd9a79f72cbbb6f60e298d9
aolsenjazz <13665641+aolsenjazz@users.noreply.github.com>
2025-12-02 15:09:14 +0000

    Auto-commit updated styles

--- design-tokens/build/web/tailwind.config.js
+++ design-tokens/build/web/tailwind.config.js
@@ -367,7 +367,7 @@ module.exports = {
         color: {
           feedback: {
             success: "#5ccf2d",
-            onSuccess: "#f7f8f8",
+            onSuccess: "#f5f5f6",
             info: "#338dc4",
             onInfo: "#f3f7fc",
             warning: "#fcb03b",

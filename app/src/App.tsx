1447696bc31047bde63c401884674f15d4b492e6
Alexander Olsen <aolsenjazz@gmail.com>
2025-12-06 18:08:11 -0500

    import the tailwind css file in app.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,28 +1,28 @@
 import { useAppDispatch, useAppSelector } from '@/app/hooks';
 import { increaseBy, increment } from '@/features/counter/counter-slice';
 
-import './App.css';
+import './index.css';
 
 function App() {
   const dispatch = useAppDispatch();
   const count = useAppSelector((state) => state.counter.value);
 
   return (
-    <main className='app'>
+    <main className="app">
       <h1>Vault.fm Boilerplate</h1>
       <p>React + TypeScript + Vite + Redux Toolkit</p>
 
-      <div className='button-group'>
-        <button className='btn' onClick={() => dispatch(increment())}>
+      <div className="button-group">
+        <button className="btn" onClick={() => dispatch(increment())}>
           +1
         </button>
 
-        <button className='btn' onClick={() => dispatch(increaseBy(5))}>
+        <button className="btn" onClick={() => dispatch(increaseBy(5))}>
           +5
         </button>
       </div>
 
-      <p className='count'>
+      <p className="count">
         Count: <strong>{count}</strong>
       </p>
     </main>

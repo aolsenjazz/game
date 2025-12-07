b04d608570cd248a0313344919eea82a9b0d5727
Alexander Olsen <aolsenjazz@gmail.com>
2025-12-07 14:17:08 -0500

    Capacitor scaffolding

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alexanderolsen.game',
+  appName: 'Game',
+  webDir: 'dist',
+};
+
+export default config;

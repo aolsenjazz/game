50eb2e1013c804b48bf7a99f271554b493dfa54c
Alexander Olsen <aolsenjazz@gmail.com>
2026-01-03 10:33:14 -0500

    Bootstrap capawesome

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -4,6 +4,12 @@ const config: CapacitorConfig = {
   appId: 'com.alexanderolsen.game',
   appName: 'Game',
   webDir: 'dist',
+  plugins: {
+    LiveUpdate: {
+      appId: 'b742c78c-5772-4fae-bb80-5462eeb5cb2a',
+      autoUpdateStrategy: 'background',
+    },
+  },
 };
 
 export default config;
